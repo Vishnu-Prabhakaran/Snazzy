922add0ddcbdebdd9037c3013599e82d72f577a2
Vishnu Prabhakaran <vprabha@deakin.edu.au>
2019-12-01 23:34:58 +1100

    Shop Page - Collection preview & collection component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
@@ -15,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/hats" component={HatsPage} />
+          <Route path="/shop" component={ShopPage} />
         </Switch>
       </BrowserRouter>
     </div>
