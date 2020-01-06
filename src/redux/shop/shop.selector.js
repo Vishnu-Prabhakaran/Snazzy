d4ee3bc7d4bfa9d92d01c6c10f028cd3b177bb66
Vishnu Prabhakaran <vprabha@deakin.edu.au>
2020-01-06 20:10:23 +1100

    Collection component - error

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -6,3 +6,10 @@ export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
 );
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    // Find collection.id matching the url parameter of our collection if map
+    collections => collections[collectionUrlParam]
+  );
