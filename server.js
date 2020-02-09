b135396f09c6e34e5055883c018beffdbb4f9538
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-02-09 23:51:31 +1100

    Payment Route

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
+// Stripe library
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 // Express
 const app = express();
 // Port - Server port will be different than local host 3000
 const port = process.env.PORT || 5000;
 
-// convert all request to json
+// Convert all request to json
 app.use(bodyParser.json());
 // Encod url
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,4 +33,21 @@ if (process.env.NODE_ENV === "production") {
 app.listen(port, error => {
   if (error) throw error;
   console.log("Server running on port" + port);
+});
+
+// Payment route
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "aud"
+  };
+  // Charges
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
