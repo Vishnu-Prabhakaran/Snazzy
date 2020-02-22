bd851e8af005235e92d8ac7e93f3f65523628082
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-02-22 16:34:35 +1100

    Profiler

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,24 @@
-import React from "react";
-import Directory from "../../components/directory/directory.component";
+import React, { Profiler } from 'react';
+import Directory from '../../components/directory/directory.component';
 
 // Styled component
-import { HomePageContainer } from "./homepage.styles";
+import { HomePageContainer } from './homepage.styles';
 
+// Wrap the profiler to the component that you want to profile
 const HomePage = () => (
   <HomePageContainer>
-    <Directory />
+    <Profiler
+      id='Directory'
+      onRender={(id, phase, actualDuration) => {
+        console.log({
+          id,
+          phase,
+          actualDuration
+        });
+      }}
+    >
+      <Directory />
+    </Profiler>
   </HomePageContainer>
 );
 
